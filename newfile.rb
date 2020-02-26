0e5420f363441cd423f4ccf94679da9d4b9beeb6
JonathanDowell <mr.jonathandowell@gmail.com>
2020-02-25 18:59:02 -0600

    tom finally made it to the store

--- newfile.rb
+++ newfile.rb
@@ -1,3 +1,6 @@
 puts "Tom attempted to go to  the stroe. On the way, Tom fell down. Tom never made it to the store. Tom walked back home slowly."
 
 puts "Tom returned home. Tom decided to try gain tomorrow."
+
+puts "Tom made finally made it to the store."
+
